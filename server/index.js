f72ca7cdedcbd3ce744279bd9cf7d9d0041a67b3
Georgios.D <georgios_97@hotmail.com>
2023-03-02 11:34:37 +0100

    Init

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+const app = express();
+import mongoose from "mongoose";
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+const channelsSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 30,
+        },
+    },
+    { timestamps: true }
+);
+
+const Channel = mongoose.model("channels", channelsSchema);
+
+app.get("/channels", async (req, res) => {
+    const channels = await Channel.find();
+    res.send(channels);
+});
+
+app.listen(3000, async () => {
+    await mongoose.connect(
+        "mongodb+srv://GeorgiosD:georgiosd@cluster0.ywdaimb.mongodb.net/?retryWrites=true&w=majority"
+    );
+});
