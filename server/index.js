388ff9513bba4f4c681f696278c1ff6820b039c5
Georgios.D <georgios_97@hotmail.com>
2023-03-09 17:29:04 +0100

    Started on messages

--- server/index.js
+++ server/index.js
@@ -28,6 +28,56 @@ app.get("/channels", async (req, res) => {
     res.send(channels);
 });
 
+const messageSchema = new mongoose.Schema(
+    {
+        text: {
+            type: String,
+            required: true,
+            minLength: 1,
+        },
+        user: {
+            name: {
+                type: String,
+                required: true,
+            },
+            image: {
+                type: String,
+                required: true,
+            },
+        },
+        channelId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "channels",
+        },
+    },
+    { timestamps: true }
+);
+
+const Message = mongoose.model("messages", messageSchema);
+
+app.get("/channels/:id", async (req, res) => {
+    await Message.insertMany([
+        {
+            text: "First Message",
+            user: {
+                name: "John Doe",
+                image: "https://avatars.githubusercontent.com/u/25107756?v=4",
+            },
+            channelId: "64007b739f9f1b9023ecc194",
+        },
+        {
+            text: "Second Message",
+            user: {
+                name: "John Doe",
+                image: "https://avatars.githubusercontent.com/u/25107756?v=4",
+            },
+            channelId: "64007b739f9f1b9023ecc194",
+        },
+    ]);
+    const messages = await Message.find();
+    res.send(messages);
+});
+
 app.listen(3000, async () => {
     await mongoose.connect(
         "mongodb+srv://GeorgiosD:georgiosd@cluster0.ywdaimb.mongodb.net/?retryWrites=true&w=majority"
